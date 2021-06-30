a74ebcd86931d27d3c35d8820c15e92e3512f0c0
efredborg <e.fredborg@hotmail.com>
2021-06-30 14:38:16 +0200

    sdf

--- explore_fits.py
+++ explore_fits.py
@@ -1,8 +1,12 @@
 import numpy as np
-print(1)
-from astropy import fits
-print(2)
-#filename = '/mn/stornext/d9/iris/data/level2/2014/05/21/20140521_114758_3820258168'
+from astropy.io import fits
+import matplotlib.pyplot as plt
+filename = 'iris_l2_20140521_114758_3820258168_raster_t000_r00487.fits'
 
-#hdu = fits.open(filename)
-#hdu.info()
+hdu = fits.open(filename)
+hdu.info()
+ext1 = hdu[1].data
+
+
+plt.plot(np.arange(ext1.shape[-1]), ext1[0,0,:])
+plt.show()
