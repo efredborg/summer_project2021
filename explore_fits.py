f5e4a8bcbb2182855429884cf0a2f51a2ff9c83d
efredborg <e.fredborg@hotmail.com>
2021-06-30 16:36:21 +0200

    sf

--- explore_fits.py
+++ explore_fits.py
@@ -5,8 +5,3 @@ filename = 'iris_l2_20140521_114758_3820258168_raster_t000_r00487.fits'
 
 hdu = fits.open(filename)
 hdu.info()
-ext1 = hdu[1].data
-
-
-plt.plot(np.arange(ext1.shape[-1]), ext1[0,0,:])
-plt.show()
