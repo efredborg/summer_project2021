649bc257d484cb2081175e84c94e03d852e769e0
efredborg <e.fredborg@hotmail.com>
2021-08-09 09:38:01 +0200

    producing fft plots for p_max=60

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # summer_project2021
 Containing files used/produced while working as a scientific assistant at the Institute of theoretical astrophysics at UiO during the summer of 2021.
+
+
+The .pro files contain IDL code designed to run at my personal computer at the Institute for theoretical astrophysics at UiO. They perform an Iris2 inversion and save the computed light curves at optical depth, log(tau) = -5. Further oscillation analysis is performed on the produced light curves in python. The results from the wavelet and FFT analysis are found in the "plots" folder.
--- code/period_analysis.py
+++ code/period_analysis.py
@@ -2,14 +2,8 @@ import numpy as np
 from wavelet import *
 
 LCs = ['temperature', 'vlos', 'vturb', '$n_e$']
-data_folder = 'data_files/'
+data_folder = '/home/efredborg/Documents/summer_project2021/data_files/'
 data = np.loadtxt(data_folder+'vars.dat')
-#data = np.loadtxt(data_folder+'siint_400.dat')
-#wavelet(data, LC_type = 'siint400')
-#fourier(data, LC_type = 'siint400', P_max = 32)
-#temp = data[:,0]
-#wavelet(temp, LC_type = 'temperature')
-#fourier(temp, LC_type= 'temperature', P_max = 32)
 for i, LC_name in enumerate(LCs):
     LC = data[:,i]
     wavelet_func(LC, LC_type = LC_name, plot_LC=True, P_max = 60)
